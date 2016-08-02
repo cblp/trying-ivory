1dfdb98bd86c26e1a86f84eb795290e97dc604e5
Yuriy Syrovetskiy <cblp@cblp.su>
2016-08-02 18:04:11 +0300

    [m] style

--- src/Main.hs
+++ src/Main.hs
@@ -12,9 +12,25 @@ import qualified Ivory.Compile.C.CmdlineFrontend as C
 import           Ivory.Language
 import           Ivory.Language.Proc
 
+-- | 'call_' a procedure with no arguments. Useful for variadic procedures.
+-- call0_  :: IvoryCall_ ('[] ':-> ()) eff (Ivory eff ())
+--         => Def        ('[] ':-> ()) ->  (Ivory eff ())
+-- call0_ = call_
+
+-- | 'call_' a procedure with 1 argument exactly. Useful for variadic procedures.
+-- call1_  :: IvoryCall_ ('[a] ':-> ()) eff (a -> Ivory eff ())
+--         => Def        ('[a] ':-> ()) ->  (a -> Ivory eff ())
+-- call1_ = call_
+
+-- | 'call_' a procedure with 2 arguments exactly. Useful for variadic procedures.
 call2_  :: IvoryCall_ ('[a, b] ':-> ()) eff (a -> b -> Ivory eff ())
-        => Def ('[a, b] ':-> ()) -> (a -> b -> Ivory eff ())
+        => Def        ('[a, b] ':-> ()) ->  (a -> b -> Ivory eff ())
 call2_ = call_
+
+-- | 'call_' a procedure with 3 arguments exactly. Useful for variadic procedures.
+-- call3_  :: IvoryCall_ ('[a, b, c] ':-> ()) eff (a -> b -> c -> Ivory eff ())
+--         => Def        ('[a, b, c] ':-> ()) ->  (a -> b -> c -> Ivory eff ())
+-- call3_ = call_
 
 printf :: ProcType (args ':-> ()) => Def ((IString ': args) ':-> ())
 printf = importProc "printf" "stdio.h"
@@ -23,20 +39,19 @@ class CPrint a where
     cprint :: a -> Ivory eff ()
 
 instance CPrint Uint32 where
-    cprint i = call2_ printf "%u\n" i
+    cprint = call2_ printf "%u\n"
 
 instance CPrint IChar where
-    cprint c = call2_ printf "%c\n" c
+    cprint = call2_ printf "%c\n"
 
 instance CPrint IString where
-    cprint s = call2_ printf "%s\n" s
+    cprint = call2_ printf "%s\n"
 
 cmain :: Def ('[] ':-> ())
 cmain = proc "main" $ body $ do
     cprint (1 :: Uint32)
     cprint (char 'a')
     cprint ("hello" :: IString)
-    retVoid
 
 hello :: Module
 hello = package "hello" $ do
